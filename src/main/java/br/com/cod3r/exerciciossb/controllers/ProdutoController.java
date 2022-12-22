6289c048a9951a9ba4bf7dffbdcff16e2b419ae6
Milena Vieira <97436683+milenavieira@users.noreply.github.com>
2022-12-22 15:21:24 -0300

    Add files via upload

---
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -0,0 +1,25 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+
+import br.com.cod3r.exerciciossb.model.entities.Produto;
+import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @PostMapping
+    public @ResponseBody Produto novoProduto(
+            @RequestParam String nome,
+            @RequestParam double preco,
+            @RequestParam double desconto) {
+        Produto produto = new Produto(nome, preco, desconto);
+        produtoRepository.save(produto);
+        return produto;
+    }
+}
---
+++ src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.cod3r.exerciciossb.model.repositories;
+
+import br.com.cod3r.exerciciossb.model.entities.Produto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+
+
+}
